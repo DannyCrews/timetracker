1e8a363f4480973c3d17f464fe0c85957ee04b7d
Dan Crews <dancrews@me.com>
2014-03-21 16:37:05 -0400

    Add validations to user and project models

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,7 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, :through => :works
 
+  validates :name, prewsence: true
+
   scope :lowdefaultrate, -> { where("default_rate < 100")}
 end
